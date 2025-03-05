f75771bebf6e4e09ebdb76be6a7d470718cd739b
kishanraj04 <kishuraj1111@gmail.com>
2025-03-05 06:22:57 +0530

    showing limited stocke product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,8 @@ import { saveAllProduct } from "../../store/productSlice.js";
 import { getRandomData } from "../../utils/getRandomData.js";
 import Card2 from "../components/card/Card2.jsx";
 import Card3 from "../components/card/Card3.jsx";
+import Card4 from "../components/card/Card4.jsx";
+import Card1 from "../components/card/Card1.jsx";
 
 function Home() {
   const navigate = useNavigate();
@@ -55,8 +57,42 @@ function Home() {
         <div className="w-[100%] flex justify-center items-center">
           <div className="flex flex-wrap justify-center gap-3 w-[98%]">
             {allProduct &&
-              getRandomData(allProduct, 1).map((product, idx) => (
+              getRandomData(allProduct, 2).map((product, idx) => (
                 <Card3 key={idx} item={product} />
+              ))}
+          </div>
+        </div>
+      </div>
+
+
+       {/*Trending product */}
+       <div className="w-[100%]">
+        <div>
+          <h1>Trending Product</h1>
+        </div>
+
+        <div className="w-[100%] flex justify-center items-center">
+          <div className="flex flex-wrap justify-center gap-3 w-[98%]">
+            {allProduct &&
+              getRandomData(allProduct, 3).map((product, idx) => (
+                <Card4 key={idx} item={product} />
+              ))}
+          </div>
+        </div>
+      </div>
+
+
+      {/*Trending product */}
+      <div className="w-[100%]">
+        <div>
+          <h1>Limited Stock</h1>
+        </div>
+
+        <div className="w-[100%] flex justify-center items-center">
+          <div className="flex flex-wrap justify-center gap-3 w-[98%]">
+            {allProduct &&
+              getRandomData(allProduct, 0).map((product, idx) => (
+                <Card1 key={idx} item={product} />
               ))}
           </div>
         </div>
