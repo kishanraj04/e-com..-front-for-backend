05b4c7a9d72f0c850486fda20b82c9c6819883f6
kishanraj04 <kishuraj1111@gmail.com>
2025-03-12 23:29:59 +0530

    creating handler for form submission

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 
@@ -12,6 +12,20 @@ function Contact() {
   //   console.log("lo ",isLoggedInUserStatus);
   //   if(isLoggedInUserStatus==false) {navigate('/')}
   // },[])
+  const nameRef = useRef(null)
+  const emailRef = useRef(null)
+  const messageRef = useRef(null)
+
+
+  const handleContactSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log(
+      nameRef.current?.value, 
+      emailRef.current?.value, 
+      messageRef.current?.value
+    );
+  };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-6 bg-gray-100">
@@ -34,6 +48,7 @@ function Contact() {
             type="text"
             className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Enter your name"
+            ref={nameRef}
           />
         </div>
 
@@ -43,6 +58,7 @@ function Contact() {
             type="email"
             className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Enter your email"
+            ref={emailRef}
           />
         </div>
 
@@ -52,12 +68,14 @@ function Contact() {
             rows="4"
             className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Write your message..."
+            ref={messageRef}
           ></textarea>
         </div>
 
         <button
           type="submit"
           className="w-full bg-blue-600 text-white font-bold py-3 rounded-lg hover:bg-blue-700 transition duration-300"
+          onClick={(e)=>handleContactSubmit(e)}
         >
           Send Message
         </button>
